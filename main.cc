ee888a4781c1e701ce99b82f0d67de83f0f99079
Chengke <wongck@pku.edu.cn>
2020-05-13 21:01:43 +0800

    add stopwatch

--- main.cc
+++ main.cc
@@ -9,24 +9,28 @@
 #include "impl_simd_mmx.h"
 #include "impl_simd_sse2.h"
 #include "impl_simd_avx.h"
+#include "stopwatch.h"
 
 template <typename Impl>
 void do_work(Impl impl) {
+    Stopwatch stopwatch;
 	YUV420 image1;
     image1.read_from_yuv("dem1.yuv");
     YUV420 output = impl.AlphaBlending(image1, 128);
-    output.write_to_yuv("output.yuv");
+    // output.write_to_yuv("output.yuv");
 
     image1.free();
     output.free();
+
+    std::cout << stopwatch.getElapsedInMicroseconds() / 1000 << " ms " << std::endl;
 }
 
 int main() {
     try {
         
-        // do_work(ImplGeneral());
-        // do_work(ImplSimdMMX());
-        // do_work(ImplSimdSSE2());
+        do_work(ImplGeneral());
+        do_work(ImplSimdMMX());
+        do_work(ImplSimdSSE2());
         do_work(ImplSimdAVX());
 
 
